875f149d0752749f5fc51f8a59b355856299ae56
ildar <ildar@debian.ildar>
2024-03-10 11:50:14 +0100

    guessing game added

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+```
+Disclaimer
+Every symbol, line, file, and folder contained in the present repository was created with one and the only purpose of learning new skills and obtain knowledge.
+The repository may contain files that were never meant to be neither sold, nor stolen.
+```
+
 # rustacean
-A few small pet projects, recommended by The Book
+
+## In general
+
+The goal of the repo is to show a few small pet projects, recommended by The Book.<br>
+Repeating these projects helps in understanding the core abilities of the language as well as learning unmentioned details.<br>
+
+## Projects
+- guessing game
---
+++ guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use std::io::Write;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        print!("Enter your guess: ");
+        std::io::stdout().flush().unwrap();
+
+        let mut guess = String::new();
+
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}

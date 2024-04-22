e611a1795a0885e8e475f82a8e1374fe1dee9592
ilRECh <ildar.rch@gmail.com>
2024-04-22 09:44:05 +0200

    extracting "run" functionality

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,19 +1,29 @@
 // for handling arguments that are passed from a command line
-use std::env;
-use std::fs;
+use std::{ env, fs, process, error::Error };
 
 fn main() {
-    // get the arguments
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args)
+        .unwrap_or_else(|err_str| {
+            println!("{err_str}");
+            process::exit(1);
+        });
 
     println!("Searching for \"{}\" in the file named \"{}\"", config.regex, config.filename);
 
-    let content = fs::read_to_string(config.filename)
-        .expect("The file must be accessbile");
+    if let Err(err) = run(config) {
+        println!("{err}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
 
     println!("{content}");
+
+    Ok(())
 }
 
 struct Config {
@@ -22,15 +32,14 @@ struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Self {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 2 {
-            println!("Not enugh arguments.\nUsage: minigrep <regex> <filename>");
-            std::process::exit(1);
+            return Err("Not enough arguments.\nUsage: minigrep <regex> <filename>");
         }
 
-        Config {
+        Ok(Config {
             regex: args[0].clone(),
             filename: args[1].clone()
-        }
+        })
     }
 }
